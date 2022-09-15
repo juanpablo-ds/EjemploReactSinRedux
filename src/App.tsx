ce723ad161fc2ffd70846faaa48f6461ec297ef4
Eduard Sol <e.sol@talentum.com.es>
2022-09-15 15:11:58 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import SearchBar from './components/searchBar';
 import './App.css';
 
 function App() {
+  
   return (
     <div className="App">
+      <SearchBar/>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          App de pruebas
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </header> 
     </div>
   );
 }
---
+++ src/components/searchBar.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+//componente a exportar
+const SearchBar = () => {
+ return <div>Barra de Busqueda <input /></div>
+}
+
+//exportando el componente deseado
+export default SearchBar;
